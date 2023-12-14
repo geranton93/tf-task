3555313d524c29d1138f1f3f839dfaac2ba0389b
geranton93 <Xlive93@yandex.ru>
2023-12-14 15:17:14 +0200

    added modules, variables, outputs

--- main.tf
+++ main.tf
@@ -1,13 +1,41 @@
-module "gke_cluster" {
-  source         = "git@github.com:geranton93/tf-google-gke-cluster.git"
-  GOOGLE_REGION  = var.GOOGLE_REGION
-  GOOGLE_PROJECT = var.GOOGLE_PROJECT
-  GKE_NUM_NODES  = var.GKE_NUM_NODES
+# module "gke_cluster" {
+#   source         = "git@github.com:geranton93/tf-google-gke-cluster.git"
+#   GOOGLE_REGION  = var.GOOGLE_REGION
+#   GOOGLE_PROJECT = var.GOOGLE_PROJECT
+#   GKE_NUM_NODES  = var.GKE_NUM_NODES
+# }
+
+module "kind_cluster" {
+  source = "github.com/den-vasyliev/tf-kind-cluster"
 }
 
-terraform {
-  backend "gcs" {
-    bucket = "terraform-status-bucket-00d23e84-28ef-47b8-8eac-5dc1151360d5"
-    prefix = "terraform/state"
-  }
+module "tls_private_key" {
+  source = "github.com/den-vasyliev/tf-hashicorp-tls-keys"
+
+  algorithm   = var.algorithm
+  ecdsa_curve = var.ecdsa_curve
 }
+
+module "github_repository" {
+  source                   = "github.com/den-vasyliev/tf-github-repository"
+  github_owner             = var.GITHUB_OWNER
+  github_token             = var.GITHUB_TOKEN
+  repository_name          = var.FLUX_GITHUB_REPO
+  public_key_openssh       = module.tls_private_key.public_key_openssh
+  public_key_openssh_title = "flux"
+}
+
+module "flux_bootstrap" {
+  source            = "github.com/den-vasyliev/tf-fluxcd-flux-bootstrap"
+  github_repository = "${var.GITHUB_OWNER}/${var.FLUX_GITHUB_REPO}"
+  private_key       = module.tls_private_key.private_key_pem
+  config_path       = module.kind_cluster.kubeconfig
+  github_token      = var.GITHUB_TOKEN
+}
+
+# terraform {
+#   backend "gcs" {
+#     bucket = "terraform-status-bucket-00d23e84-28ef-47b8-8eac-5dc1151360d5"
+#     prefix = "terraform/state"
+#   }
+# }
--- variables.tf
+++ variables.tf
@@ -1,11 +1,39 @@
-variable "GOOGLE_REGION" {
-  description = "Google Cloud region"
+# variable "GOOGLE_REGION" {
+#   description = "Google Cloud region for GKE cluster"
+#   type        = string
+# }
+
+# variable "GOOGLE_PROJECT" {
+#   description = "Google Cloud project for GKE cluster"
+#   type        = string
+# }
+
+# variable "GKE_NUM_NODES" {
+#   description = "Number of nodes for GKE cluster"
+#   type        = number
+# }
+
+variable "algorithm" {
+  description = "Algorithm for TLS private key"
+  type        = string
 }
 
-variable "GOOGLE_PROJECT" {
-  description = "Google Cloud project ID"
+variable "ecdsa_curve" {
+  description = "ECDSA curve for TLS private key"
+  type        = string
 }
 
-variable "GKE_NUM_NODES" {
-  description = "Number of nodes in GKE cluster"
+variable "GITHUB_OWNER" {
+  description = "GitHub repository owner"
+  type        = string
+}
+
+variable "GITHUB_TOKEN" {
+  description = "GitHub personal access token"
+  type        = string
+}
+
+variable "FLUX_GITHUB_REPO" {
+  description = "GitHub repository name for Flux"
+  type        = string
 }
